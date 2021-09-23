a9694fb65674548fcdd93fde2faac641e2cf4469
Nurudeen Ajayi <nurudeenajayi@Nurudeens-MacBook-Pro.local>
2021-09-23 13:47:25 +0100

    Refactored Generic view to Viewsets

--- MyProject/api_basic/urls.py
+++ MyProject/api_basic/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path, include
 # from .views import article_list, article_detail, ArticleAPIView, ArticleDetails
-from .views import ArticleAPIView, ArticleDetails, GenericAPIView
+from .views import ArticleAPIView, ArticleDetails, GenericAPIView, ArticleViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+# register out routes
+router.register('article', ArticleViewSet, basename='article')
 
 urlpatterns = [
+    path('api/viewset/', include(router.urls)),
+    path('api/viewset/<int:pk>/', include(router.urls)),
     path('api/articles/', ArticleAPIView.as_view()),
     # path('api/articles/', article_list),
     # path('api/article-detail/<int:pk>/', article_detail)
