abe4b9c00261dc515d8b96c27655fceea0a469d8
Nurudeen Ajayi <nurudeenajayi@Nurudeens-MacBook-Pro.local>
2021-09-23 13:17:52 +0100

    Authentication with token

--- MyProject/api_basic/views.py
+++ MyProject/api_basic/views.py
@@ -10,7 +10,10 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework import generics
 from rest_framework import mixins
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
+
 
 class GenericAPIView(
         generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin, mixins.UpdateModelMixin,
@@ -20,6 +23,9 @@ class GenericAPIView(
     queryset = Article.objects.all()
 
     lookup_field = 'id'
+    # authentication_classes = [SessionAuthentication, BasicAuthentication]
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def get(self, request, id = None):
         if id:
