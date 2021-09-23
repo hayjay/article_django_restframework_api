a98e243c94dff6170d38c729c7a8458d1dd22c68
Nurudeen Ajayi <nurudeenajayi@Nurudeens-MacBook-Pro.local>
2021-09-23 13:00:35 +0100

    From Class based views to Generic Api views

--- MyProject/api_basic/urls.py
+++ MyProject/api_basic/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 # from .views import article_list, article_detail, ArticleAPIView, ArticleDetails
-from .views import ArticleAPIView, ArticleDetails
+from .views import ArticleAPIView, ArticleDetails, GenericAPIView
 
 urlpatterns = [
     path('api/articles/', ArticleAPIView.as_view()),
     # path('api/articles/', article_list),
     # path('api/article-detail/<int:pk>/', article_detail)
-    path('api/article-detail/<int:id>/', ArticleDetails.as_view())
+    path('api/article-detail/<int:id>/', ArticleDetails.as_view()),
+    path('api/generic/articles/<int:id>/', GenericAPIView.as_view()),
+    # path('api/generic/articles/', GenericAPIView.as_view()),
+
     # ArticleDetails
 ]
